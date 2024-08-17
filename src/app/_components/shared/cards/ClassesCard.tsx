114dbc850b24026ababffba5b8f5adc44cbd8dd9
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2024-08-17 21:19:47 +0500

    feat: classes fetching by groups

--- src/app/_components/shared/cards/ClassesCard.tsx
+++ src/app/_components/shared/cards/ClassesCard.tsx
@@ -9,29 +9,36 @@ import {
 } from "~/components/ui/card";
 import Link from "next/link";
 
-
 export const ClassesCard = async () => {
-  const classses = await api.class.getClasses();
+  const classesData = await api.class.getGroupedClasses();
 
   return (
-    <section className="grid grid-cols-4 gap-4">
-      {classses.map((classProps) => (
-        <Card key={classProps.classId}>
-          <CardHeader>
-            <CardTitle>{classProps.className}</CardTitle>
-            <CardDescription>{classProps.classSlug}</CardDescription>
-          </CardHeader>
-
-          <CardFooter>
-            <Button className="w-full" asChild>
-              <Link
-                href={`/student/academics/class_student/${classProps.classId}`}
-              >
-                Detail
-              </Link>
-            </Button>
-          </CardFooter>
-        </Card>
+    <section className="flex flex-col gap-4">
+      {classesData.map((classProps, index) => (
+        <div key={index} className="flex flex-col gap-4">
+          <h1 className="text-white font-bold text-3xl bg-green-600 rounded-md shadow-lg p-4">{classProps.category}</h1>
+          <div className="grid grid-cols-4 gap-3">
+          {classProps.classes.map((classData, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle>{classData.className}</CardTitle>
+                <CardDescription>{classData.classSlug}</CardDescription>
+                <CardDescription>{classData.category}</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Button className="w-full" asChild>
+                  <Link
+                    href={`/student/academics/class_student/${classData.classId}`}
+                  >
+                    Detail
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+          </div>
+          
+        </div>
       ))}
     </section>
   );
